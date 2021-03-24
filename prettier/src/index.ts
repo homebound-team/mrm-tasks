bf1aef5a9279d7ee4908ed48120719de54de9e32
Stephen Haberman <shaberman@homebound.com>
2021-03-24 17:26:12 -0500

    feat: Initial prettier.

---
+++ prettier/src/index.ts
@@ -0,0 +1,20 @@
+import { packageJson, install } from "mrm-core";
+
+/** Homebounds prettier setup. */
+function task() {
+  const pkg = packageJson();
+  pkg.setScript("format", "prettier --write 'src/**/*.{ts,js,tsx,jsx}'");
+  // We prefer the `prettier` key in `package.json` b/c:
+  // - it avoids extra files
+  // - we can use merge
+  // - its unlikely we'll need comments
+  pkg.merge({
+    prettier: {
+      trailingComma: "all",
+      printWidth: 120,
+    },
+  });
+  install("prettier");
+}
+
+export = task;
